ed44f5ce99cdea32f7a07ee4424b57cad6f7dffb
Tim Reynolds <tim@cubingusa.org>
2020-02-11 19:48:48 -0500

    Use backend link from environment.

--- app/src/app/header/header.component.ts
+++ app/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService, LoginState } from '../auth.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +9,7 @@ import { AuthService, LoginState } from '../auth.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  environment = environment;
 
   loginState: Observable<LoginState>;
 
